fc5a0f656990b226818fe612eb19e21eae214a70
민진 김 <kindcode@gmail.com>
2019-08-22 10:02:32 +0900

    v1.5 sql connection test succes

--- ArtActionProject/Program.cs
+++ ArtActionProject/Program.cs
@@ -17,7 +17,7 @@ namespace ArtActionProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new LoginForm());
-            Application.Run(new GuidForm());
+            Application.Run(new LoginForm());
         }
     }
 }
